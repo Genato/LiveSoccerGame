cef4eb188cd8e5b5e2ad9536b87d954ff99ce886
Genato <druidhr@gmail.com>
2017-07-30 17:02:30 +0200

    Added new javascript file "VisualEffects_jQuery" just for visual effects. (Purpose is to split visal effects logic from standard business logic tat handels usual data). Made some new visual modification in CSS file to top menu bar.

---
+++ src/LiveSoccerGame/wwwroot/js/VisualEffects_jquery.js
@@ -0,0 +1,25 @@
+﻿/*PreierLegaueIcon effects*/
+$('#PreierLegaueIcon').hover(makeBigger, returnToOriginalSize);
+
+function makeBigger() {
+    $(this).css({ "width": '+=4px' });
+    $('.Container_Override').css({ "text-shadow": "1px 1px white", "margin-left": "+=30px" });
+}
+
+function returnToOriginalSize() {
+    $(this).css({ width: "" });
+    $('.Container_Override').css({ "text-shadow": "", "margin-left": "-=30px" });
+}
+/*PreierLegaueIcon effects*/
+
+/*Top menu bar text effects*/
+$('.TextEffects').hover(TextEffectsHover, TextEffectsUnHover);
+
+function TextEffectsHover() {
+    $(this).css({ "text-shadow": "1.1px 1.1px rgb(128, 170, 255)", "font-size": "+=1px" });
+}
+
+function TextEffectsUnHover() {
+    $(this).css({ "text-shadow": "", "font-size": "" });
+}
+/*Top menu bar text effects*/
