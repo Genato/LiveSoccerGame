2a48bf9b23d7b732801b03ec3d1f7e8952125514
Renato.Katalenic <druidhr@gmail.com>
2017-08-28 00:17:31 +0200

    Made separate function for Moving Ball image.

--- src/LiveSoccerGame/wwwroot/js/VisualEffects_jquery.js
+++ src/LiveSoccerGame/wwwroot/js/VisualEffects_jquery.js
@@ -1,12 +1,14 @@
 ﻿/*PreierLegaueIcon effects*/
 $('.PreierLegaueIcon').hover(makeBigger, returnToOriginalSize);
 
-function makeBigger() {
+function makeBigger()
+{
     $(this).css({ "width": '+=4px' });
     $('.Container_Override').css({ "text-shadow": "1px 1px white", "margin-left": "+=5px" });
 }
 
-function returnToOriginalSize() {
+function returnToOriginalSize()
+{
     $(this).css({ width: "" });
     $('.Container_Override').css({ "text-shadow": "", "margin-left": "-=5px" });
 }
@@ -15,30 +17,33 @@ function returnToOriginalSize() {
 /*Top menu bar text effects*/
 $('.TextEffects').hover(TextEffectsHover, TextEffectsUnHover);
 
-function TextEffectsHover() {
+function TextEffectsHover()
+{
     $(this).css({ "text-shadow": "1.1px 1.1px rgb(128, 170, 255)", "font-size": "+=1px" });
 }
 
-function TextEffectsUnHover() {
+function TextEffectsUnHover()
+{
     $(this).css({ "text-shadow": "", "font-size": "" });
 }
 /*Top menu bar text effects*/
 
-// Ball image on load 
+// Ball image on load - Start
 $("#_Container").ready(function () {
-    var _ContainerPosition = $("#_Container").offset();
-    var _ContainerHeight = $("#_Container").height();
-    var _ContainerWidth = $("#_Container").width();
-    var ballTopPosition = (_ContainerHeight / 2) + _ContainerPosition.top;
-    var ballLeftPosition = (_ContainerWidth / 2) + _ContainerPosition.left;
-    $(".Ball").css({ "left": +ballLeftPosition, "top": +ballTopPosition });
+    MoveBall();
 });
 
 $(window).resize(function () {
+    MoveBall();
+});
+
+function MoveBall()
+{
     var _ContainerPosition = $("#_Container").offset();
     var _ContainerHeight = $("#_Container").height();
     var _ContainerWidth = $("#_Container").width();
     var ballTopPosition = (_ContainerHeight / 2) + _ContainerPosition.top;
     var ballLeftPosition = (_ContainerWidth / 2) + _ContainerPosition.left;
     $(".Ball").css({ "left": +ballLeftPosition, "top": +ballTopPosition });
-});
+}
+// Ball image on load - Start
